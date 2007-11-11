0cc1478979da571eef056b37c7f0df40bb04975a
Petr Baudis <pasky@ucw.cz>
2007-11-12 00:59:42 +0100

    Runtime engine selection support: zzgo -e

--- zzgo.c
+++ zzgo.c
@@ -1,5 +1,7 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #include "board.h"
@@ -11,8 +13,38 @@
 int main(int argc, char *argv[])
 {
 	struct board *b = board_init();
-	//struct engine *e = engine_random_init();
-	struct engine *e = engine_montecarlo_init();
+	enum { E_RANDOM, E_MONTECARLO } engine = E_MONTECARLO;
+
+	int opt;
+	while ((opt = getopt(argc, argv, "e:")) != -1) {
+		switch (opt) {
+			case 'e':
+				if (!strcasecmp(optarg, "random")) {
+					engine = E_RANDOM;
+				} else if (!strcasecmp(optarg, "montecarlo")) {
+					engine = E_MONTECARLO;
+				} else {
+					fprintf(stderr, "%s: Invalid -e argument %s\n", argv[0], optarg);
+					exit(1);
+				}
+				break;
+			default: /* '?' */
+				fprintf(stderr, "Usage: %s [-e random|montecarlo]\n",
+						argv[0]);
+				exit(1);
+		}
+	}
+
+	struct engine *e;
+	switch (engine) {
+		case E_RANDOM:
+		default:
+			e = engine_random_init(); break;
+		case E_MONTECARLO:
+			e = engine_montecarlo_init(); break;
+
+	}
+
 	char buf[256];
 	while (fgets(buf, 256, stdin)) {
 		//fprintf(stderr, "IN: %s", buf);
