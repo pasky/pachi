6707284597fb9429f946302e000c532058989de8
Petr Baudis <pasky@ucw.cz>
2007-11-12 02:04:38 +0100

    Montecarlo: Add support for 'debug' engine argument

--- montecarlo/montecarlo.c
+++ montecarlo/montecarlo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "board.h"
 #include "engine.h"
@@ -11,6 +12,11 @@
  * it plays MC_GAMES random games from that board; the move with the biggest
  * number of winning games gets played. */
 
+/* Pass me arguments like a=b,c=d,...
+ * Supported arguments:
+ * debug=DEBUG_LEVEL
+ */
+
 
 #if 0 // board 19
 #define MC_GAMES	1000
@@ -21,9 +27,14 @@
 #endif
 
 
+struct montecarlo {
+	int debug_level;
+};
+
+
 /* Stolen from the random engine. */
 static void
-random_move(struct board *b, enum stone color, struct coord *coord)
+random_move(struct montecarlo *mc, struct board *b, enum stone color, struct coord *coord)
 {
 #define TRIES	MC_GAMELEN
 	struct move m;
@@ -48,7 +59,7 @@ random_move(struct board *b, enum stone color, struct coord *coord)
 }
 
 static float
-play_random_game(struct board *b, enum stone color, int moves)
+play_random_game(struct montecarlo *mc, struct board *b, enum stone color, int moves)
 {
 	struct board b2;
 	board_copy(&b2, b);
@@ -56,9 +67,13 @@ play_random_game(struct board *b, enum stone color, int moves)
 	struct coord coord;
 	int passes = 0;
 	while (moves-- && passes < 2) {
-		random_move(&b2, color, &coord);
+		random_move(mc, &b2, color, &coord);
 		struct move m = { coord, color };
-		//char *cs = coord2str(coord); fprintf(stderr, "%s %s\n", stone2str(color), cs); free(cs);
+		if (mc->debug_level > 6) {
+			char *cs = coord2str(coord);
+			fprintf(stderr, "%s %s\n", stone2str(color), cs);
+			free(cs);
+		}
 		board_play_nocheck(&b2, &m);
 		if (is_pass(coord))
 			passes++;
@@ -66,6 +81,9 @@ play_random_game(struct board *b, enum stone color, int moves)
 			passes = 0;
 		color = stone_other(color);
 	}
+
+	if (mc->debug_level > 5)
+		board_print(&b2, stderr);
 
 	float score = board_fast_score(&b2);
 
@@ -75,7 +93,7 @@ play_random_game(struct board *b, enum stone color, int moves)
 
 /* positive: player-to-play wins more, negative: player-to-play loses more */
 static int
-play_many_random_games_from(struct board *b, struct move *m)
+play_many_random_games_from(struct montecarlo *mc, struct board *b, struct move *m)
 {
 	struct board b2;
 	board_copy(&b2, b);
@@ -89,8 +107,9 @@ play_many_random_games_from(struct board *b, struct move *m)
 
 	int i;
 	for (i = 0; i < MC_GAMES; i++) {
-		float score = play_random_game(&b2, stone_other(m->color), gamelen);
-		//fprintf(stderr, "--- game result: %f\n", score);
+		float score = play_random_game(mc, &b2, stone_other(m->color), gamelen);
+		if (mc->debug_level > 4)
+			fprintf(stderr, "--- game result: %f\n", score);
 		balance += (score > 0 ? 1 : -1) * (stone_other(m->color) == S_WHITE ? 1 : -1);
 	}
 
@@ -102,6 +121,7 @@ play_many_random_games_from(struct board *b, struct move *m)
 static struct coord *
 montecarlo_genmove(struct engine *e, struct board *b, enum stone color)
 {
+	struct montecarlo *mc = e->data;
 	struct move m;
 	m.color = color;
 
@@ -117,9 +137,11 @@ montecarlo_genmove(struct engine *e, struct board *b, enum stone color)
 		if (!board_valid_move(b, &m, true))
 			continue;
 
-		//fprintf(stderr, "[%d,%d] random\n", x, y);
-		int score = - play_many_random_games_from(b, &m);
-		//fprintf(stderr, "\tscore %d\n", score);
+		if (mc->debug_level > 3)
+			fprintf(stderr, "[%d,%d] random games\n", x, y);
+		int score = - play_many_random_games_from(mc, b, &m);
+		if (mc->debug_level > 3)
+			fprintf(stderr, "\tscore %d\n", score);
 		if (score > top_score) {
 			top_score = score;
 			top_coord = m.coord;
@@ -132,13 +154,36 @@ montecarlo_genmove(struct engine *e, struct board *b, enum stone color)
 struct engine *
 engine_montecarlo_init(char *arg)
 {
+	struct montecarlo *mc = calloc(1, sizeof(struct montecarlo));
 	struct engine *e = calloc(1, sizeof(struct engine));
 	e->name = "MonteCarlo Engine";
 	e->comment = "I'm playing in Monte Carlo. When we both pass, I will consider all the stones on the board alive.";
 	e->genmove = montecarlo_genmove;
+	e->data = mc;
 
-	if (arg)
-		fprintf(stderr, "MonteCarlo: I support no engine arguments.\n");
+	mc->debug_level = 1;
+
+	if (arg) {
+		char *optspec, *next = arg;
+		while (*next) {
+			optspec = next;
+			next += strcspn(next, ",");
+			if (*next) { *next++ = 0; } else { *next = 0; }
+
+			char *optname = optspec;
+			char *optval = strchr(optspec, '=');
+			if (optval) *optval++ = 0;
+
+			if (!strcasecmp(optname, "debug")) {
+				if (optval)
+					mc->debug_level = atoi(optval);
+				else
+					mc->debug_level++;
+			} else {
+				fprintf(stderr, "MonteCarlo: Invalid engine argument %s\n", optname);
+			}
+		}
+	}
 
 	return e;
 }
