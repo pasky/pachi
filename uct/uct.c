53d7960c26ff8921da14eef5aa8c8edd89869f18
Petr Baudis <pasky@ucw.cz>
2008-03-08 00:27:36 +0100

    UCT: Adjust default hint rates slightly

--- uct/uct.c
+++ uct/uct.c
@@ -172,9 +172,9 @@ uct_state_init(char *arg)
 	u->gamelen = MC_GAMELEN;
 	u->explore_p = 0.2;
 	u->expand_p = 2;
-	u->mc.capture_rate = 90;
-	u->mc.atari_rate = 90;
-	u->mc.cut_rate = 80;
+	u->mc.capture_rate = 100;
+	u->mc.atari_rate = 100;
+	u->mc.cut_rate = 50;
 	// Looking at the actual playouts, this just encourages MC to make
 	// stupid shapes.
 	u->mc.local_rate = 0;
