b536e67879a3e9b2ce35516b9ab8f24a90a3c499
Petr Baudis <pasky@ucw.cz>
2008-09-17 19:05:02 +0200

    random.c: seed -> pmseed (naming conflict with zzgo.c)

--- random.c
+++ random.c
@@ -5,22 +5,22 @@
 
 /* Simple Park-Miller */
 
-static unsigned long seed = 29264;
+static unsigned long pmseed = 29264;
 
 void
 fast_srandom(unsigned long seed_)
 {
-	seed = seed_;
+	pmseed = seed_;
 }
 
 unsigned long
 fast_random(unsigned int max)
 {
 	unsigned long hi, lo;
-	lo = 16807 * (seed & 0xffff);
-	hi = 16807 * (seed >> 16);
+	lo = 16807 * (pmseed & 0xffff);
+	hi = 16807 * (pmseed >> 16);
 	lo += (hi & 0x7fff) << 16;
 	lo += hi >> 15;
-	seed = (lo & 0x7fffffff) + (lo >> 31);
-	return ((seed & 0xffff) * max) >> 16;
+	pmseed = (lo & 0x7fffffff) + (lo >> 31);
+	return ((pmseed & 0xffff) * max) >> 16;
 }
