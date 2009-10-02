525cf27ee1748d2aaab94d104e2ab5d0d530ca4a
Petr Baudis <pasky@ucw.cz>
2009-10-02 16:17:10 +0200

    uct_prior_one(): Split to one function per heuristic

--- uct/prior.c
+++ uct/prior.c
@@ -18,18 +18,18 @@
 
 
 void
-uct_prior_one(struct uct *u, struct tree_node *node, struct prior_map *map, coord_t c)
+uct_prior_even(struct uct *u, struct tree_node *node, struct prior_map *map, coord_t c)
 {
-	/* Initialization of UCT values based on prior knowledge */
-
 	/* Q_{even} */
 	/* This may be dubious for normal UCB1 but is essential for
 	 * reading stability of RAVE, it appears. */
-	if (u->even_eqex) {
-		map->prior[c].playouts += u->even_eqex;
-		map->prior[c].wins += u->even_eqex / 2;
-	}
+	map->prior[c].playouts += u->even_eqex;
+	map->prior[c].wins += u->even_eqex / 2;
+}
 
+void
+uct_prior_eye(struct uct *u, struct tree_node *node, struct prior_map *map, coord_t c)
+{
 	/* Discourage playing into our own eyes. However, we cannot
 	 * completely prohibit it:
 	 * #######
@@ -41,26 +41,32 @@ uct_prior_one(struct uct *u, struct tree_node *node, struct prior_map *map, coor
 		map->prior[c].playouts += u->eqex;
 		map->prior[c].wins += map->parity > 0 ? 0 : u->eqex;
 	}
+}
 
+void
+uct_prior_b19(struct uct *u, struct tree_node *node, struct prior_map *map, coord_t c)
+{
 	/* Q_{b19} */
 	/* Specific hints for 19x19 board - priors for certain edge distances. */
-	if (u->b19_eqex) {
-		int d = coord_edge_distance(c, map->b);
-		if (d == 1 || d == 3) {
-			/* The bonus applies only with no stones in immediate
-			 * vincinity. */
-			if (!board_stone_radar(map->b, c, 2)) {
-				/* First line: -eqex */
-				/* Third line: +eqex */
-				int v = d == 1 ? -1 : 1;
-				map->prior[c].playouts += u->b19_eqex;
-				map->prior[c].wins += map->parity * v > 0 ? u->b19_eqex : 0;
-			}
+	int d = coord_edge_distance(c, map->b);
+	if (d == 1 || d == 3) {
+		/* The bonus applies only with no stones in immediate
+		 * vincinity. */
+		if (!board_stone_radar(map->b, c, 2)) {
+			/* First line: -eqex */
+			/* Third line: +eqex */
+			int v = d == 1 ? -1 : 1;
+			map->prior[c].playouts += u->b19_eqex;
+			map->prior[c].wins += map->parity * v > 0 ? u->b19_eqex : 0;
 		}
 	}
+}
 
+void
+uct_prior_grandparent(struct uct *u, struct tree_node *node, struct prior_map *map, coord_t c)
+{
 	/* Q_{grandparent} */
-	if (u->gp_eqex && node->parent && node->parent->parent) {
+	if (node->parent && node->parent->parent) {
 		struct tree_node *gpp = node->parent->parent;
 		for (struct tree_node *ni = gpp->children; ni; ni = ni->sibling) {
 			/* Be careful not to emphasize too random results. */
@@ -70,23 +76,39 @@ uct_prior_one(struct uct *u, struct tree_node *node, struct prior_map *map, coor
 			}
 		}
 	}
+}
 
+void
+uct_prior_playout(struct uct *u, struct tree_node *node, struct prior_map *map, coord_t c)
+{
 	/* Q_{playout-policy} */
-	if (u->policy_eqex) {
-		int assess = 0;
-		if (u->playout->assess) {
-			struct move m = { c, map->to_play };
-			assess = u->playout->assess(u->playout, map->b, &m, u->policy_eqex);
-		}
-		if (assess) {
-			map->prior[c].playouts += abs(assess);
-			/* Good moves for enemy are losses for us.
-			 * We will properly maximize this in the UCB1
-			 * decision. */
-			assess *= map->parity;
-			if (assess > 0) map->prior[c].wins += assess;
-		}
+	int assess = 0;
+	if (u->playout->assess) {
+		struct move m = { c, map->to_play };
+		assess = u->playout->assess(u->playout, map->b, &m, u->policy_eqex);
 	}
+	if (assess) {
+		map->prior[c].playouts += abs(assess);
+		/* Good moves for enemy are losses for us.
+		 * We will properly maximize this in the UCB1
+		 * decision. */
+		assess *= map->parity;
+		if (assess > 0) map->prior[c].wins += assess;
+	}
+}
+
+void
+uct_prior_one(struct uct *u, struct tree_node *node, struct prior_map *map, coord_t c)
+{
+	if (u->even_eqex)
+		uct_prior_even(u, node, map, c);
+	uct_prior_eye(u, node, map, c);
+	if (u->b19_eqex)
+		uct_prior_b19(u, node, map, c);
+	if (u->gp_eqex)
+		uct_prior_grandparent(u, node, map, c);
+	if (u->policy_eqex)
+		uct_prior_playout(u, node, map, c);
 }
 
 void
