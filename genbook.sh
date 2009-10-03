5904e36cc52a05553c16084910053d3075ad0fbe
Petr Baudis <pasky@ucw.cz>
2009-10-03 18:53:21 +0200

    genbook.sh: Allow passing custom size and engine options

--- genbook.sh
+++ genbook.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-rm uctbook-9-7.5.pachitree
+size="$1"
+opts="$2"
+[ -n "$size" ] || size=9
+[ -z "$opts" ] || opts=",$opts"
+
+rm uctbook-$size-7.5.pachitree
 n=0
 genbook1()
 {
 	echo "[#$n:$1]"
 	n=$((n+1))
-	echo -e 'boardsize 9\nclear_board\nkomi 7.5\nuct_genbook b' | ./zzgo games=1000000,policy=ucb1amaf:explore_p=$1
+	echo -e 'boardsize '$size'\nclear_board\nkomi 7.5\nuct_genbook b' | ./zzgo "games=1000000,explore_p=$1$opts"
 }
 genbook1 0.1
 genbook1 0.2
