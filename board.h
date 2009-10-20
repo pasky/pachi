c9bd346cb67490e61d7e551cfbd987633f84f814
Petr Baudis <pasky@ucw.cz>
2009-10-21 01:03:24 +0200

    Board: Support for engine-specific board state

--- board.h
+++ board.h
@@ -127,6 +127,10 @@ struct board {
 	/* Basic ko check */
 	struct move ko;
 
+	/* Engine-specific state; persistent through board development,
+	 * is reset only at clear_board. */
+	void *es;
+
 
 	/* --- PRIVATE DATA --- */
 
@@ -181,6 +185,7 @@ void board_done_noalloc(struct board *board);
 void board_done(struct board *board);
 /* size here is without the S_OFFBOARD margin. */
 void board_resize(struct board *board, int size);
+/* The caller must take care of releasing board.es first. */
 void board_clear(struct board *board);
 
 struct FILE;
--- engine.h
+++ engine.h
@@ -8,12 +8,14 @@ struct engine;
 
 typedef void (*engine_notify_play)(struct engine *e, struct board *b, struct move *m);
 typedef coord_t *(*engine_genmove)(struct engine *e, struct board *b, enum stone color);
+typedef void (*engine_done_board_state)(struct engine *e, struct board *b);
 
 struct engine {
 	char *name;
 	char *comment;
 	engine_notify_play notify_play;
 	engine_genmove genmove;
+	engine_done_board_state done_board_state;
 	void *data;
 };
 
