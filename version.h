03fdad440419c8651e104d2170ec95ad9688a1b5
Petr Baudis <pasky@ucw.cz>
2009-10-21 00:40:14 +0200

    Pachi Dosaku-devel 4.99

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef ZZGO_VERSION_H
 #define ZZGO_VERSION_H
 
-#define PACHI_VERNUM 4.0
-#define PACHI_VERNUMS "4.0"
+#define PACHI_VERNUM 4.99
+#define PACHI_VERNUMS "4.99"
 
 /* 00. Wang Zhi
  * 01. Sansa     Meijin  1612-1623
@@ -29,10 +29,10 @@
  * 19. Shuei     Meijin  1887-1907
  * 20. Shugen    6-Dan   1907-1908
  * 21. Shusai    Meijin  1908-1940 */
-#define PACHI_VERNAME "Doetsu"
+#define PACHI_VERNAME "Dosaku"
 
 /* -devel except when tagged. */
-#define PACHI_VERDEV ""
+#define PACHI_VERDEV "-devel"
 
 #define PACHI_VERSION PACHI_VERNUMS " (" PACHI_VERNAME PACHI_VERDEV ")"
 
