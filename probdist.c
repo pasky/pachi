5652fbed017eb0f3a0733fddc5b97a13e09b9373
Petr Baudis <pasky@ucw.cz>
2009-10-27 13:06:56 +0100

    Introduce probdist.*, tool for picking moves according to probability distribution

---
+++ probdist.c
@@ -0,0 +1,44 @@
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "move.h"
+#include "probdist.h"
+#include "random.h"
+
+struct probdist *
+probdist_init(struct probdist *pd, int bsize2)
+{
+	if (!pd) pd = malloc(sizeof(*pd));
+	pd->bsize2 = bsize2;
+	pd->moves = calloc(bsize2, sizeof(pd->moves[0]));
+	pd->total = 0;
+	return pd;
+}
+
+void
+probdist_add(struct probdist *pd, coord_t c, float val)
+{
+	assert(pd);
+	assert(c >= 0 && c < pd->bsize2);
+	assert(val >= 0);
+	pd->moves[c] += val;
+	pd->total += val;
+}
+
+coord_t
+probdist_pick(struct probdist *pd)
+{
+	assert(!(pd->total < 0));
+	if (pd->total < __FLT_EPSILON__)
+		return pass;
+	float stab = (float) fast_random(65536) / 65536 * pd->total;
+	float sum = 0;
+	for (coord_t c = 0; c < pd->bsize2; c++) {
+		sum += pd->moves[c];
+		if (stab < sum)
+			return c;
+	}
+	assert(0); /* wtf */
+}
---
+++ probdist.h
@@ -0,0 +1,18 @@
+#ifndef ZZGO_PROBDIST_H
+#define ZZGO_PROBDIST_H
+
+/* Tools for picking a move according to a probability distribution. */
+
+#include "move.h"
+
+struct probdist {
+	int bsize2;
+	float *moves; // [bsize2]
+	float total;
+};
+
+struct probdist *probdist_init(struct probdist *pd, int bsize2);
+void probdist_add(struct probdist *pd, coord_t c, float val);
+coord_t probdist_pick(struct probdist *pd);
+
+#endif
