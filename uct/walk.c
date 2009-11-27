3dd27efce1c118acab5063498b8abe30035b44ad
Petr Baudis <pasky@ucw.cz>
2009-11-27 22:41:24 +0100

    UCT walk: leaf assertion may not hold in parallel search

--- uct/walk.c
+++ uct/walk.c
@@ -246,7 +246,9 @@ uct_playout(struct uct *u, struct board *b, enum stone player_color, struct tree
 		struct uct_board *ub = b->es; assert(ub);
 		playout_ownermap_fill(&ub->ownermap, &b2);
 
-	} else { assert(tree_leaf_node(n));
+	} else { assert(u->parallel_tree || tree_leaf_node(n));
+		/* In case of parallel tree search, the assertion might
+		 * not hold if two threads chew on the same node. */
 		result = uct_leaf_node(u, &b2, player_color, amaf, t, n, node_color, spaces);
 	}
 
