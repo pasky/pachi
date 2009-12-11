64b5d2285760da31f17b05e4cd1dc8142cb1a9b8
Petr Baudis <pasky@ucw.cz>
2009-12-11 01:53:41 +0100

    Patternscan: Add internal state, currently w/ only debuglevel

--- patternscan/patternscan.c
+++ patternscan/patternscan.c
@@ -8,6 +8,12 @@
 #include "pattern.h"
 
 
+/* Internal engine state. */
+struct patternscan {
+	int debug_level;
+};
+
+
 static char *
 patternscan_play(struct engine *e, struct board *b, struct move *m)
 {
@@ -35,17 +41,49 @@ patternscan_genmove(struct engine *e, struct board *b, enum stone color)
 	exit(EXIT_FAILURE);
 }
 
+
+struct patternscan *
+patternscan_state_init(char *arg)
+{
+	struct patternscan *ps = calloc(1, sizeof(struct patternscan));
+
+	ps->debug_level = 1;
+
+	if (arg) {
+		char *optspec, *next = arg;
+		while (*next) {
+			optspec = next;
+			next += strcspn(next, ",");
+			if (*next) { *next++ = 0; } else { *next = 0; }
+
+			char *optname = optspec;
+			char *optval = strchr(optspec, '=');
+			if (optval) *optval++ = 0;
+
+			if (!strcasecmp(optname, "debug")) {
+				if (optval)
+					ps->debug_level = atoi(optval);
+				else
+					ps->debug_level++;
+			} else {
+				fprintf(stderr, "patternscan: Invalid engine argument %s or missing value\n", optname);
+			}
+		}
+	}
+
+	return ps;
+}
+
 struct engine *
 engine_patternscan_init(char *arg)
 {
+	struct patternscan *ps = patternscan_state_init(arg);
 	struct engine *e = calloc(1, sizeof(struct engine));
 	e->name = "PatternScan Engine";
 	e->comment = "You cannot play Pachi with this engine, it is intended for special development use - scanning of games fed to it as GTP streams for various pattern features.";
 	e->genmove = patternscan_genmove;
 	e->notify_play = patternscan_play;
-
-	if (arg)
-		fprintf(stderr, "PatternScan: I support no engine arguments\n");
+	e->data = ps;
 
 	return e;
 }
