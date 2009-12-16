0fe9c63daab8f4e18b6b3b7d4d62f9c2212eac0f
Petr Baudis <pasky@ucw.cz>
2009-12-16 01:48:03 +0100

    pattern_match_spatial(): Clarify comments

--- pattern.c
+++ pattern.c
@@ -290,7 +290,7 @@ pattern_match_spatial(struct pattern_config *pc, pattern_spec ps,
 		      struct board *b, struct move *m)
 {
 	/* XXX: This is partially duplicated from spatial_from_board(), but
-	 * building a hash instead of spatial record. */
+	 * we build a hash instead of spatial record. */
 
 	assert(pc->spat_min > 0);
 
@@ -304,7 +304,6 @@ pattern_match_spatial(struct pattern_config *pc, pattern_spec ps,
 			int y = coord_y(m->coord, b) + ptcoords[j].y;
 			if (x >= board_size(b)) x = board_size(b) - 1; else if (x < 0) x = 0;
 			if (y >= board_size(b)) y = board_size(b) - 1; else if (y < 0) y = 0;
-			/* Append point. */
 			h ^= spatial_hash_one(0, j, (*bt)[board_atxy(b, x, y)]);
 		}
 		if (d < pc->spat_min)
