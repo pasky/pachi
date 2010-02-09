4df1a6962276c8980a7639b926da6307251c5a45
Petr Baudis <pasky@ucw.cz>
2010-02-09 04:25:19 +0100

    coord2str(): Do not reuse the coord2sstr() static buffers
    This partially fixes a race where multiple threads use coord2sstr().
    coord2str() is vital for reporting the coordinate over GTP, oops!

--- move.c
+++ move.c
@@ -11,28 +11,38 @@
 
 static char asdf[] = "abcdefghjklmnopqrstuvwxyz";
 
-char *
-coord2str(coord_t c, struct board *board)
+static char *
+coord2bstr(char *buf, coord_t c, struct board *board)
 {
-	return strdup(coord2sstr(c, board));
-}
-
-char *
-coord2sstr(coord_t c, struct board *board)
-{
-	static char *b;
-	static char bl[10][4];
-	static int bi;
 	if (is_pass(c)) {
 		return "pass";
 	} else if (is_resign(c)) {
 		return "resign";
 	} else {
 		/* Some GTP servers are broken and won't grok lowercase coords */
-		b = bl[bi]; bi = (bi + 1) % 10;
-		snprintf(b, 4, "%c%d", toupper(asdf[coord_x(c, board) - 1]), coord_y(c, board));
-		return b;
+		snprintf(buf, 4, "%c%d", toupper(asdf[coord_x(c, board) - 1]), coord_y(c, board));
+		return buf;
 	}
+}
+
+/* Return coordinate in dynamically allocated buffer. */
+char *
+coord2str(coord_t c, struct board *board)
+{
+	char buf[256];
+	return strdup(coord2bstr(buf, c, board));
+}
+
+/* Return coordinate in statically allocated buffer, with some backlog for
+ * multiple independent invocations. Useful for debugging. */
+char *
+coord2sstr(coord_t c, struct board *board)
+{
+	static char *b;
+	static char bl[10][4];
+	static int bi;
+	b = bl[bi]; bi = (bi + 1) % 10;
+	return coord2bstr(b, c, board);
 }
 
 /* No sanity checking */
