23e6f038ed7c15865f03255eac164261e20f2db3
Petr Baudis <pasky@ucw.cz>
2010-06-28 16:42:30 +0200

    Probdist: Index items by coord_t instead of int

--- probdist.c
+++ probdist.c
@@ -8,8 +8,8 @@
 #include "random.h"
 #include "board.h"
 
-int
-probdist_pick(struct probdist *pd, int *ignore)
+coord_t
+probdist_pick(struct probdist *pd, coord_t *ignore)
 {
 	double total = probdist_total(pd) - PROBDIST_EPSILON;
 	assert(total >= 0);
@@ -22,16 +22,16 @@ probdist_pick(struct probdist *pd, int *ignore)
 		r++;
 		assert(r < pd->n1);
 	}
-	for (int i = r * pd->n1; i < pd->n; i++) {
+	for (coord_t c = r * pd->n1; c < pd->n; c++) {
 		//struct board b = { .size = 11 };
-		//fprintf(stderr, "[%s] %f (%f)\n", coord2sstr(i, &b), pd->items[i], stab);
-		if (*ignore && i == *ignore) {
+		//fprintf(stderr, "[%s] %f (%f)\n", coord2sstr(c, &b), pd->items[c], stab);
+		if (*ignore && c == *ignore) {
 			ignore++;
 			continue;
 		}
-		if (stab <= pd->items[i])
-			return i;
-		stab -= pd->items[i];
+		if (stab <= pd->items[c])
+			return c;
+		stab -= pd->items[c];
 	}
 
 	fprintf(stderr, "overstab %f (total %f)\n", stab, total);
--- probdist.h
+++ probdist.h
@@ -15,46 +15,47 @@
 
 struct probdist {
 	int n, n1;
-	double *items; // [n], items[i] = P(pick==i)
-	double *rowtotals; // [n1], [i] = sum of items in row i
-	double total;
+	double *items; // [bsize2], [i] = P(pick==i)
+	double *rowtotals; // [bsize], [i] = sum of items in row i
+	double total; // sum of all items
 };
 #define probdist_total(pd) ((pd)->total)
-#define probdist_one(pd, i) ((pd)->items[i])
+#define probdist_one(pd, c) ((pd)->items[c])
 /* Probability so small that it's same as zero; used to compensate
  * for probdist.total inaccuracies. */
 #define PROBDIST_EPSILON 0.05
 
-static void probdist_set(struct probdist *pd, int i, double val);
-static void probdist_mute(struct probdist *pd, int i);
+static void probdist_set(struct probdist *pd, coord_t c, double val);
+static void probdist_mute(struct probdist *pd, coord_t c);
 
 /* Pick a random item. ignore is a zero-terminated sorted array of items
  * that are not to be considered (and whose values are not in @total). */
-int probdist_pick(struct probdist *pd, int *ignore);
+coord_t probdist_pick(struct probdist *pd, coord_t *ignore);
 
 
 /* We disable the assertions here since this is quite time-critical
  * part of code, and also the compiler is reluctant to inline the
  * functions otherwise. */
 static inline void
-probdist_set(struct probdist *pd, int i, double val)
+probdist_set(struct probdist *pd, coord_t c, double val)
 {
 #if 0
-	assert(i >= 0 && i < pd->n);
+	assert(c >= 0 && c < board_size2(pd->b));
 	assert(val >= 0);
 #endif
-	pd->total += val - pd->items[i];
-	pd->rowtotals[i / pd->n1] += val - pd->items[i];
-	pd->items[i] = val;
+	pd->total += val - pd->items[c];
+	pd->rowtotals[c / pd->n1] += val - pd->items[c];
+	pd->items[c] = val;
 }
 
 /* Remove the item from the totals; this is used when you then
- * pass it in the ignore list to probdist_pick(). */
+ * pass it in the ignore list to probdist_pick(). Of course you
+ * must restore the totals afterwards. */
 static inline void
-probdist_mute(struct probdist *pd, int i)
+probdist_mute(struct probdist *pd, coord_t c)
 {
-	pd->total -= pd->items[i];
-	pd->rowtotals[i / pd->n1] -= pd->items[i];
+	pd->total -= pd->items[c];
+	pd->rowtotals[c / pd->n1] -= pd->items[c];
 }
 
 #endif
