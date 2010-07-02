42e4ccc0f95d7ccf4082afca1645bd32cd640b69
Petr Baudis <pasky@ucw.cz>
2010-07-02 22:29:39 +0200

    probdist_pick(): Fix ignoring when skipping rows

--- probdist.c
+++ probdist.c
@@ -11,7 +11,7 @@
 #include "board.h"
 
 coord_t
-probdist_pick(struct probdist *pd, coord_t *ignore)
+probdist_pick(struct probdist *restrict pd, coord_t *restrict ignore)
 {
 	double total = probdist_total(pd) - PROBDIST_EPSILON;
 	assert(total >= 0);
@@ -20,18 +20,31 @@ probdist_pick(struct probdist *pd, coord_t *ignore)
 		fprintf(stderr, "stab %f / %f\n", stab, total);
 
 	int r = 0;
+	coord_t c = 0;
 	while (stab > pd->rowtotals[r] + PROBDIST_EPSILON) {
+		if (DEBUGL(6))
+			fprintf(stderr, "[%s] skipping row %f (%f)\n", coord2sstr(c, pd->b), pd->rowtotals[r], stab);
+
 		stab -= pd->rowtotals[r];
-		r++;
-		assert(r < board_size(pd->b));
+		r++; assert(r < board_size(pd->b));
+
+		c += board_size(pd->b);
+		while (!is_pass(*ignore) && *ignore <= c)
+			ignore++;
 	}
-	for (coord_t c = r * board_size(pd->b); c < board_size2(pd->b); c++) {
+
+	for (; c < board_size2(pd->b); c++) {
 		if (DEBUGL(6))
 			fprintf(stderr, "[%s] %f (%f)\n", coord2sstr(c, pd->b), pd->items[c], stab);
+
+		assert(is_pass(*ignore) || c <= *ignore);
 		if (c == *ignore) {
+			if (DEBUGL(6))
+				fprintf(stderr, "ignored\n");
 			ignore++;
 			continue;
 		}
+
 		if (stab <= pd->items[c])
 			return c;
 		stab -= pd->items[c];
