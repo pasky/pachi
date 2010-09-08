01825b69295771e99adde3a915d6279cc9fb6b85
Jean-loup Gailly <jloup@gailly.net>
2010-09-08 16:20:04 +0200

    Allow compilation on MacOS which does not support -march=native, clock_gettime and the __thread attribute.

--- random.c
+++ random.c
@@ -5,6 +5,8 @@
 
 /* Simple Park-Miller */
 
+#ifndef NO_THREAD_LOCAL
+
 static __thread unsigned long pmseed = 29264;
 
 void
@@ -40,3 +42,60 @@ fast_frandom(void)
 	p.ul = (((pmseed *= 16807) & 0x007fffff) - 1) | 0x3f800000;
 	return p.f - 1.0f;
 }
+
+#else
+
+/* Thread local storage not supported through __thread,
+ * use pthread_getspecific() instead. */
+
+#include <pthread.h>
+
+static pthread_key_t seed_key;
+
+static void __attribute__((constructor))
+random_init(void)
+{
+	pthread_key_create(&seed_key, NULL);
+	fast_srandom(29264UL);
+}
+
+void
+fast_srandom(unsigned long seed_)
+{
+	pthread_setspecific(seed_key, (void *)seed_);
+}
+
+unsigned long
+fast_getseed(void)
+{
+	return (unsigned long)pthread_getspecific(seed_key);
+}
+
+uint16_t
+fast_random(unsigned int max)
+{
+	unsigned long pmseed = (unsigned long)pthread_getspecific(seed_key);
+	unsigned long hi, lo;
+	lo = 16807 * (pmseed & 0xffff);
+	hi = 16807 * (pmseed >> 16);
+	lo += (hi & 0x7fff) << 16;
+	lo += hi >> 15;
+	pmseed = (lo & 0x7fffffff) + (lo >> 31);
+	pthread_setspecific(seed_key, (void *)pmseed);
+	return ((pmseed & 0xffff) * max) >> 16;
+}
+
+float
+fast_frandom(void)
+{
+	/* Construct (1,2) IEEE float from our random integer */
+	/* http://rgba.org/articles/sfrand/sfrand.htm */
+	unsigned long pmseed = (unsigned long)pthread_getspecific(seed_key);
+	pmseed *= 16807;
+	union { unsigned long ul; float f; } p;
+	p.ul = ((pmseed & 0x007fffff) - 1) | 0x3f800000;
+	pthread_setspecific(seed_key, (void *)pmseed);
+	return p.f - 1.0f;
+}
+
+#endif
