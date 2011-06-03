a8a710aae9cc4272709dea3a1b06bae4be703f4c
Petr Baudis <pasky@ucw.cz>
2011-06-03 12:01:29 +0200

    libmap_queue_mqpick(): Pick preferentially only with p=1-epsilon (eps=0.2 now)

--- libmap.h
+++ libmap.h
@@ -156,9 +156,9 @@ libmap_queue_mqpick(struct libmap_hash *lm, struct libmap_mq *q)
 		goto pick;
 
 	/* Pick random move, up to a simple check - if a move has tactical
-	 * rating <0.5, prefer another. (FIXME: Up to epsilon?) */
+	 * rating lower than threshold, prefer another. */
 	p = pp = fast_random(q->mq.moves);
-	do {
+	if (fast_random(100) >= 20) do {
 		int pm = p % q->mq.moves;
 		struct move m = { .coord = q->mq.move[pm], .color = q->color[pm] };
 		struct move_stats *ms = libmap_move_stats(lm, q->group[pm].hash, m);
