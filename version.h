c1ce4bd4f89546dddbe2aa4b2b3d7268424c3315
Petr Baudis <pasky@ucw.cz>
2011-10-23 15:32:24 +0200

    Pachi Hakugen 9.00

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef PACHI_VERSION_H
 #define PACHI_VERSION_H
 
-#define PACHI_VERNUM 8.99
-#define PACHI_VERNUMS "8.99"
+#define PACHI_VERNUM 9.00
+#define PACHI_VERNUMS "9.00"
 
 /* 00. Wang Zhi
  * 01. Sansa     Meijin  1612-1623
@@ -32,7 +32,7 @@
 #define PACHI_VERNAME "Hakugen"
 
 /* -devel except when tagged. */
-#define PACHI_VERDEV "-devel"
+#define PACHI_VERDEV ""
 
 #define PACHI_VERSION PACHI_VERNUMS " (" PACHI_VERNAME PACHI_VERDEV ")"
 
