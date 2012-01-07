98e0b37d96f2ecdd147929858078af9127a44b8f
Petr Baudis <pasky@ucw.cz>
2012-01-07 17:43:16 +0100

    pattern_bayes_gen.sh: New pattern processing script

---
+++ tools/pattern_bayes_gen.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# pattern_bayes_gen: Generate pattern probability table from a SGF collection
+
+(for i in "$@"; do echo $i >&2; tools/sgf2gtp.pl <$i; done) |
+	./pachi -d 0 -e patternscan competition |
+	perl -nle '
+		BEGIN { use List::MoreUtils qw(uniq); }
+
+		s/^= // or next;
+		chomp;
+		s/\) \(/),(/g;
+		@a = split(/,/);
+
+		$win = shift @a;
+		$choices{$win}++;
+		for (uniq @a) {
+			$counts{$_}++;
+		}
+
+		END {
+			for (keys %counts) {
+				$p{$_} = $choices{$_} / $counts{$_};
+			}
+			for (sort { $p{$b} <=> $p{$a} } keys %p) {
+				printf("%.3f %d %d %s\n", $p{$_}, $choices{$_}, $counts{$_}, $_);
+			}
+		}'
