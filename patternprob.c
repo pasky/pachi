8d89c02113402652d9c91a80cd2540521ab47038
Petr Baudis <pasky@ucw.cz>
2012-01-07 21:27:57 +0100

    Pattern Probability Table: Introduce basic infrastructure

---
+++ patternprob.c
@@ -0,0 +1,59 @@
+#define DEBUG
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "board.h"
+#include "debug.h"
+#include "pattern.h"
+#include "patternsp.h"
+#include "patternprob.h"
+
+
+struct pattern_pdict *
+pattern_pdict_init(char *filename, struct pattern_config *pc)
+{
+	if (!filename)
+		filename = "patterns.prob";
+	FILE *f = fopen(filename, "r");
+	if (!f) {
+		if (DEBUGL(1))
+			fprintf(stderr, "No pattern probtable, will not use learned patterns.\n");
+		return NULL;
+	}
+
+	struct pattern_pdict *dict = calloc2(1, sizeof(*dict));
+	dict->pc = pc;
+	dict->table = calloc2(pc->spat_dict->nspatials + 1, sizeof(*dict->table));
+
+	char sbuf[1024];
+	while (fgets(sbuf, sizeof(sbuf), f)) {
+		struct pattern_prob *pb = calloc2(1, sizeof(*pb));
+		int c, o;
+
+		char *buf = sbuf;
+		if (buf[0] == '#') continue;
+		while (isspace(*buf)) buf++;
+		while (!isspace(*buf)) buf++; // we recompute the probability
+		while (isspace(*buf)) buf++;
+		c = strtol(buf, &buf, 10);
+		while (isspace(*buf)) buf++;
+		o = strtol(buf, &buf, 10);
+		pb->prob = (floating_t) c / o;
+		while (isspace(*buf)) buf++;
+		str2pattern(buf, &pb->p);
+
+		uint32_t spi = pattern2spatial(dict, &pb->p);
+		if (!dict->table[spi]) {
+			dict->table[spi] = pb;
+		} else {
+			struct pattern_prob *ppb = dict->table[spi];
+			while (ppb->next) ppb = ppb->next;
+			ppb->next = pb;
+		}
+	}
+
+	fclose(f);
+	return dict;
+}
