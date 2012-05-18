ac7d787b08e14f324bb30ef51f268a6ff65c09f3
Petr Baudis <pasky@ucw.cz>
2012-05-18 16:49:48 +0200

    fast_frandom() always returns float, not floating_t
    Noticed by Michael Williams.

--- ownermap.c
+++ ownermap.c
@@ -61,6 +61,7 @@ board_ownermap_judge_groups(struct board *b, struct board_ownermap *ownermap, st
 		if (!g) continue;
 
 		enum point_judgement pj = board_ownermap_judge_point(ownermap, c, judge->thres);
+		// assert(judge->gs[g] == GS_NONE || judge->gs[g] == pj);
 		if (pj == PJ_UNKNOWN) {
 			/* Fate is uncertain. */
 			judge->gs[g] = GS_UNKNOWN;
