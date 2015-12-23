6dc025441393feffb350631dca6f7059a92a5fb0
lemonsqueeze <lemonsqueeze@gmx.com>
2015-12-23 23:58:07 +0100

    is_bad_selfatari(): fixes + handle nakade with outside liberties
    - Allow nakade on group with outside liberties when created shape is dead
    - setup_nakade_or_snapback() fixes
      mainly was missing a check whether self-atariing group can connect instead.

--- tactics/nakade.h
+++ tactics/nakade.h
@@ -11,4 +11,7 @@
  * Returns pass if the area is not a nakade shape or not internal. */
 coord_t nakade_point(struct board *b, coord_t around, enum stone color);
 
+/* big eyespace can be reduced to one eye */
+bool nakade_dead_shape(struct board *b, coord_t around, enum stone color);
+
 #endif
