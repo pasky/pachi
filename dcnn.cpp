a8ddcc95847574392eb3d1c32249772fd18ab67a
xlvector <xlvector@gmail.com>
2016-04-18 19:10:30 +0200

    use Detlef's 54% dcnn

--- dcnn.cpp
+++ dcnn.cpp
@@ -45,8 +45,8 @@ dcnn_init()
 		return;
 
 	struct stat s;	
-	const char *model_file =   "movepredict.prototxt";
-	const char *trained_file = "movepredict.caffemodel";
+	const char *model_file =   "golast19.prototxt";
+	const char *trained_file = "golast.trained";
 	if (stat(model_file, &s) != 0  ||  stat(trained_file, &s) != 0) {
 		if (DEBUGL(1))
 			fprintf(stderr, "No dcnn files found, will not use dcnn code.\n");
@@ -67,31 +67,41 @@ void
 dcnn_get_moves(struct board *b, enum stone color, float result[])
 {
 	assert(real_board_size(b) == 19);
+
 	int size = 19;
-	float *data = new float[2 * size * size];
-	
-	if (color == S_BLACK)
-		for (int j = 0; j < size; j++)
-			for (int k = 0; k < size; k++) {
-				coord_t c = coord_xy(b, j + 1, k + 1);
-				int offsetb = j * size + k;
-				int offsetw = size * size + size * j + k;
-				data[offsetb] = (board_at(b, c) == S_BLACK ? 1.0 : 0.0);
-				data[offsetw] = (board_at(b, c) == S_WHITE ? 1.0 : 0.0);
-			}
-	
-	if (color == S_WHITE) {
-		for (int j = 0; j < size; j++)
-			for (int k = 0; k < size; k++) {
-				coord_t c = coord_xy(b, j + 1, k + 1);
-				int offsetb = j * size + k;
-				int offsetw = size * size + size * j + k;
-				data[offsetb] = (board_at(b, c) == S_WHITE ? 1.0 : 0.0);
-				data[offsetw] = (board_at(b, c) == S_BLACK ? 1.0 : 0.0);
-			}
+	int dsize = 13 * size * size;
+	float *data = new float[dsize];
+	for (int i = 0; i < dsize; i++) 
+		data[i] = 0.0;
+
+	for (int j = 0; j < size; j++) {
+		for(int k = 0; k < size; k++) {
+			int p = size * j + k;
+			coord_t c = coord_xy(b, j+1, k+1);
+			group_t g = group_at(b, c);
+			enum stone bc = board_at(b, c);
+			int libs = board_group_info(b, g).libs - 1;
+			if (libs > 3) libs = 3;
+			if (bc == S_NONE) 
+				data[8*size*size + p] = 1.0;
+			else if (bc == color)
+				data[(0+libs)*size*size + p] = 1.0;
+			else if (bc == stone_other(color))
+				data[(4+libs)*size*size + p] = 1.0;
+			
+			if (c == b->last_move.coord)
+				data[9*size*size + p] = 1.0;
+			else if (c == b->last_move2.coord)
+				data[10*size*size + p] = 1.0;
+			else if (c == b->last_move3.coord)
+				data[11*size*size + p] = 1.0;
+			else if (c == b->last_move4.coord)
+				data[12*size*size + p] = 1.0;
+			
+		}
 	}
-	
-	Blob<float> *blob = new Blob<float>(1,2,size,size);
+
+	Blob<float> *blob = new Blob<float>(1,13,size,size);
 	blob->set_cpu_data(data);
 	vector<Blob<float>*> bottom;
 	bottom.push_back(blob);
