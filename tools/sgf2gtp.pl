c42c17a4638b835b91b11b1cb1af7c58883f046f
lemonsqueeze <lemonsqueeze@gmx.com>
2017-04-04 12:02:05 +0200

    tools: sgf2gtp.pl: handle handicap, sanity checks
    abort on variations or setup stones

--- tools/sgf2gtp.pl
+++ tools/sgf2gtp.pl
@@ -17,17 +17,24 @@
 
 use warnings;
 
-my $genmove;
-if ($ARGV[0] and $ARGV[0] eq '-g') {
-	shift @ARGV;
-	$genmove = 1;
+my $abcd = "abcdefghijklmnopqrstuvwxyz";
+my $size = 19;
+
+# Convert sgf coord to gtp
+sub sgf2gtp
+{
+    my ($coord) = @_;
+    my ($x, $y) = split //, $coord;
+    ($x ge 'i') and $x++;
+    $y = $size - index($abcd, $y);
+    return "$x$y";
 }
 
+my $genmove;
+if ($ARGV[0] and $ARGV[0] eq '-g') {  shift @ARGV;  $genmove = 1;  }
+
 my $maxmoves;
-if ($ARGV[0] and $ARGV[0] eq '-n') {
-	shift @ARGV;
-	$maxmoves = shift @ARGV;
-}
+if ($ARGV[0] and $ARGV[0] eq '-n') {  shift @ARGV;  $maxmoves = shift @ARGV;  }
 
 if ($ARGV[0]) {
 	open STDIN, "$ARGV[0]" or die "$ARGV[0]: $!";
@@ -36,46 +43,68 @@ if ($ARGV[0]) {
 }
 
 local $/ = undef; my $sgf = <>;
-my $size = ($sgf =~ /SZ\[(\d+)\]/)[0];
-$size ||= 19;
+$sgf =~ s/\\]//gs;        # remove escaped brackets (in comments)
 $sgf =~ s/\bC\[.*?\]//gs; # no comments
 #$sgf =~ s/\).*//gs; # cut at end of principal branch
 
+my @m = split(/;/, $sgf);
+if (shift @m ne "(") {  die "doesn't look like valid sgf, aborting\n";  }
+my $h = shift @m;    # game header
+$h =~ s/[ \n\t]//gs; # no whitespaces
+
+if ($h =~ /SZ\[(\d+)\]/) {  $size = $1;  } else {  warn "WARNING: no board size in game header ...\n";  }
 print "boardsize " . $size;
 print "clear_board";
-if ($sgf =~ s/\bKM\[([\d.]+)\]//gs) {
-	print "komi $1";
-}
-if ($sgf =~ s/\bHA\[(\d+)\]//gs and $1 > 0) {
-	print "fixed_handicap $1";
+if ($h =~ s/\bKM\[([\d.]+)\]//gs)         {  print "komi $1";  }
+
+my $handicap = 0;
+my $set_free_handicap = "set_free_handicap";
+if ($h =~ s/\bHA\[(\d+)\]//gs and $1 > 0) {  $handicap = $1;   }
+
+
+# Handicap stones passed as setup stones ?
+if ($handicap && $h =~ m/AB((\[\w\w\])+)/)
+{
+    my $stones = $1;   $stones =~ s/\[//g;
+    foreach my $c (split(/\]/, $stones)) {
+	$set_free_handicap .= " " . sgf2gtp($c);
+	if (!--$handicap)  {  print "$set_free_handicap";  }
+	if ($handicap < 0) {  die "shouldn't happen";  }
+    }
 }
 
-my $abcd = "abcdefghijklmnopqrstuvwxyz";
+sub play
+{
+    my ($color, $coord) = @_;
+    if ($coord eq "") {  print "play $color pass"; return;  }
+
+    my $c = sgf2gtp($coord);
+
+    # Handicap stones passed as regular moves ?
+    if ($handicap) {
+	if ($color ne "B") { die "handicap stones are not black !\n"; }
+	$set_free_handicap .= " $c";
+	if (!--$handicap) { print "$set_free_handicap"; }
+	return;
+    }
+    
+    print "play $color $c";
+}
+
 my $movenum = 0;
 my $last_color = 'w';
 
-my @m = split /;/, $sgf;
 foreach (@m) {
-	$maxmoves and $movenum >= $maxmoves and last;
+    $maxmoves and $movenum >= $maxmoves and last;
 
-	if (/^([BW])\[\]/) {
-		$last_color = $1;
-		$movenum++;
-		print "play $1 pass";
-		next;
-	}
-	unless (/^([BW])\[(\w\w)\]/) {
-		next;
-	}
+    if (/\($/)                {  die "this script doesn't handle variations, aborting.\n"; }
+    if (/(AB|AW|AE)\[\w\w\]/) {  die "this script doesn't handle setup stones, aborting.\n"; }
 
-	my ($color, $coord) = ($1, $2);
-	my ($x, $y) = split //, $coord;
-	($x ge 'i') and $x++;
-	$y = $size - index($abcd, $y);
-
-	$last_color = $color;
+    if (/^([BW])\[()\]/ || /^([BW])\[(\w\w)\]/) {
+	$last_color = $1;
 	$movenum++;
-	print "play $color $x$y";
+	play($1,$2);
+    }
 }
 
 if ($genmove) {
