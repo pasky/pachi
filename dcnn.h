a2a62939c7d769c9cba734c09e79b5b2be4aed25
Stanislaw Halik <sthalik@misaki.pl>
2018-10-13 15:31:10 +0200

    require macro stubs to have a non-empty expansion

--- dcnn.h
+++ dcnn.h
@@ -33,10 +33,10 @@ coord2dcnn_idx(coord_t c, struct board *b)
 #else
 
 
-#define disable_dcnn()
-#define require_dcnn()     die("dcnn required but not compiled in, aborting.\n")
-#define using_dcnn(b)  0
-#define dcnn_init(b)
+#define disable_dcnn()  ((void)0)
+#define require_dcnn()  die("dcnn required but not compiled in, aborting.\n")
+#define using_dcnn(b)   0
+#define dcnn_init(b)    ((void)0)
 
 
 #endif
