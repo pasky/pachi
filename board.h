pasky/pachi

commit 2da3fd0ec15e682cadd47debc7b4083feae6c47b

board: board_size() redefinition
before:
  board_size(b)        padded board size, ie 21 for 19x19
  real_board_size(b)   real board size,   ie 19 for 19x19
  board_size2(b)       (board_size(b) * board_size(b))

now:
  board_rsize(b)       real board size,   ie 19 for 19x19
  board_stride(b)      padded board size, ie 21 for 19x19
  board_max_coords(b)  (board_stride(b) * board_stride(b))

api: board, joseki, move
     anything that mentions size is *real* board size

board.h
@@ -33,7 +33,7 @@ struct ownermap;
 /* Maximum supported board size. (Without the S_OFFBOARD edges.) */
 #define BOARD_MAX_SIZE 19
 
-#define BOARD_MAX_COORDS  ((BOARD_MAX_SIZE+2) * (BOARD_MAX_SIZE+2) )
+#define BOARD_MAX_COORDS  ((BOARD_MAX_SIZE+2) * (BOARD_MAX_SIZE+2))
 #define BOARD_MAX_MOVES   (BOARD_MAX_SIZE * BOARD_MAX_SIZE)
 #define BOARD_MAX_GROUPS  (BOARD_MAX_SIZE * BOARD_MAX_SIZE * 2 / 3)
 /* For 19x19, max 19*2*6 = 228 groups (stacking b&w stones, each third line empty) */
@@ -122,10 +122,10 @@ enum rules {
 
 /* Data shared by all boards of a given size */
 typedef struct {
-	int size;
-	int size2;                          /* size^2 */
+	int rsize;                          /* real board size     (19x19: 19) */
+	int stride;                         /* padded board size   (19x19: 21) */
+	int max_coords;                     /* stride^2 */
 	int bits2;                          /* ceiling(log2(size2)) */
-	int real_size2;                     /* real_board_size^2 */
 	
 	int nei8[8], dnei[4];               /* Iterator offsets for foreach_neighbor*() */
 	
@@ -149,7 +149,7 @@ extern board_statics_t board_statics;
  * Always call functions below if you want to change it. */
 
 typedef struct board {
-	int size;                 /* Including S_OFFBOARD margin - see below. */
+	int rsize;                 /* Real board size   (19x19: 19) */
 
 	int moves;
 	int captures[S_MAX];
@@ -217,27 +217,26 @@ FB_ONLY(hash_t history_hash)[history_hash_size]; /* Board "history" - hashes enc
 
 
 #ifdef BOARD_SIZE
-#define the_board_size()       (BOARD_SIZE + 2)
-#define board_size(b)          (BOARD_SIZE + 2)
-#define board_size2(b)         (board_size(b) * board_size(b))
-#define real_board_size2(b)    (BOARD_SIZE * BOARD_SIZE)
+#define board_rsize(b)          BOARD_SIZE
+#define the_board_rsize()       BOARD_SIZE
+#define the_board_stride()        (BOARD_SIZE + 2)
+#define board_max_coords(b)       (board_stride(b) * board_stride(b))
 #else
-#define the_board_size()       (board_statics.size)
-#define board_size(b)          ((b)->size)
-#define board_size2(b)         (board_statics.size2)
-#define real_board_size2(b)    (board_statics.real_size2)
+#define board_rsize(b)          ((b)->rsize)
+#define the_board_rsize()       (board_statics.rsize)
+#define the_board_stride()        (board_statics.stride)
+#define board_max_coords(b)       (board_statics.max_coords)
 #endif
 
-#define real_board_size(b)     (board_size(b) - 2)
-#define the_real_board_size()  (the_board_size() - 2)
+#define board_stride(b)           (board_rsize(b) + 2)
 
 
 /* This is a shortcut for taking different action on smaller and large boards 
  * (e.g. picking different variable defaults). This is of course less optimal than
  * fine-tuning dependency function of values on board size, but that is difficult
  * and possibly not very rewarding if you are interested just in 9x9 and 19x19. */
-#define board_large(b_) (board_size(b_)-2 >= 15)
-#define board_small(b_) (board_size(b_)-2 <= 9)
+#define board_large(b_) (board_rsize(b_) >= 15)
+#define board_small(b_) (board_rsize(b_) <= 9)
 
 #if BOARD_SIZE == 19
 #  define board_bits2(b_) 9
@@ -257,11 +256,11 @@ FB_ONLY(hash_t history_hash)[history_hash_size]; /* Board "history" - hashes enc
 #define last_move_previ(b, n)  ((BOARD_LAST_N + b->last_move_i - (n)) % BOARD_LAST_N)
 #define last_moven(b, n) ((b)->last_moves[last_move_previ(b, n)])
 
-#define board_at(b_, c) ((b_)->b[c])
-#define board_atxy(b_, x, y) ((b_)->b[(x) + board_size(b_) * (y)])
+#define board_at(b_, c)      ((b_)->b[c])
+#define board_atxy(b_, x, y) ((b_)->b[coord_xy(x, y)])
 
-#define group_at(b_, c) ((b_)->g[c])
-#define group_atxy(b_, x, y) ((b_)->g[(x) + board_size(b_) * (y)])
+#define group_at(b_, c)      ((b_)->g[c])
+#define group_atxy(b_, x, y) ((b_)->g[coord_xy(x, y)])
 
 /* Warning! Neighbor count is not kept up-to-date for S_NONE! */
 #define neighbor_count_at(b_, coord, color) ((b_)->n[coord].colors[(enum stone) color])
@@ -271,7 +270,6 @@ FB_ONLY(hash_t history_hash)[history_hash_size]; /* Board "history" - hashes enc
 #define immediate_liberty_count(b_, coord) (4 - neighbor_count_at(b_, coord, S_BLACK) - neighbor_count_at(b_, coord, S_WHITE) - neighbor_count_at(b_, coord, S_OFFBOARD))
 
 #define groupnext_at(b_, c) ((b_)->p[c])
-#define groupnext_atxy(b_, x, y) ((b_)->p[(x) + board_size(b_) * (y)])
 
 #define group_base(g_) (g_)
 #define group_is_onestone(b_, g_) (groupnext_at(b_, group_base(g_)) == 0)
@@ -281,18 +279,17 @@ FB_ONLY(hash_t history_hash)[history_hash_size]; /* Board "history" - hashes enc
 #define board_group_other_lib(b_, g_, l_) (board_group_info(b_, g_).lib[board_group_info(b_, g_).lib[0] != (l_) ? 0 : 1])
 
 #ifdef BOARD_HASH_COMPAT
-#define hash_at(coord, color) (*(&board_statics.h[0][0] + ((color) == S_BLACK ? board_statics.size2 : 0) + (coord)))
+#define hash_at(coord, color) (*(&board_statics.h[0][0] + ((color) == S_BLACK ? board_statics.max_coords : 0) + (coord)))
 #else
 #define hash_at(coord, color) (board_statics.h[coord][(color) == S_BLACK])
 #endif
 
 
-void board_init(board_t *b, int bsize, char *fbookfile);
-board_t *board_new(int bsize, char *fbookfile);
+void board_init(board_t *b, int size, char *fbookfile);
+board_t *board_new(int size, char *fbookfile);
 board_t *board_copy(board_t *board2, board_t *board1);
 void board_done_noalloc(board_t *board);
 void board_done(board_t *board);
-/* size here is without the S_OFFBOARD margin. */
 void board_resize(board_t *b, int size);
 void board_clear(board_t *board);
 
@@ -373,11 +370,7 @@ const char *rules2str(enum rules rules);
 #define foreach_point(board_) \
 	do { \
 		coord_t c = 0; \
-		for (; c < board_size(board_) * board_size(board_); c++)
-#define foreach_point_and_pass(board_) \
-	do { \
-		coord_t c = pass; \
-		for (; c < board_size(board_) * board_size(board_); c++)
+		for (; c < board_max_coords(board_); c++)
 #define foreach_point_end \
 	} while (0)
 
@@ -403,10 +396,10 @@ const char *rules2str(enum rules rules);
 	do { \
 		coord_t coord__ = coord_; \
 		coord_t c; \
-		c = coord__ - board_size(board_); do { loop_body } while (0); \
+		c = coord__ - board_stride(board_); do { loop_body } while (0); \
 		c = coord__ - 1; do { loop_body } while (0); \
 		c = coord__ + 1; do { loop_body } while (0); \
-		c = coord__ + board_size(board_); do { loop_body } while (0); \
+		c = coord__ + board_stride(board_); do { loop_body } while (0); \
 	} while (0)
 
 #define foreach_8neighbor(board_, coord_) \

ownermap.h
@@ -47,7 +47,6 @@ typedef struct ownermap {
 void ownermap_init(ownermap_t *ownermap);
 void board_print_ownermap(board_t *b, FILE *f, ownermap_t *ownermap);
 void ownermap_fill(ownermap_t *ownermap, board_t *b);
-void ownermap_merge(int bsize2, ownermap_t *dst, ownermap_t *src);
 
 /* Coord ownermap status: dame / black / white / unclear */
 enum point_judgement ownermap_judge_point(ownermap_t *ownermap, coord_t c, floating_t thres);
@@ -82,7 +81,7 @@ bool board_position_final_full(board_t *b, ownermap_t *ownermap,
 
 /* Don't allow passing earlier than that:
  * 19x19: 120    15x15: 56    13x13: 33    9x9: 16 */
-#define board_earliest_pass(b)  (real_board_size2(b) / (7 - 2 * real_board_size(b) / 9))
+#define board_earliest_pass(b)  (board_max_coords(b) / (7 - 2 * board_rsize(b) / 9))
 
 
 #endif

tactics/seki.h
@@ -1,8 +1,8 @@
 #ifndef PACHI_TACTICS_SEKI_H
 #define PACHI_TACTICS_SEKI_H
 
-#define MOGGY_MIDDLEGAME  (real_board_size2(b) * 10 / 25)      /*  19x19: 144 */
-#define MOGGY_ENDGAME     (real_board_size2(b) * 100 / 164)    /*  19x19: 220 */
+#define MOGGY_MIDDLEGAME  (board_rsize(b) * board_rsize(b) * 10 / 25)      /*  19x19: 144 */
+#define MOGGY_ENDGAME     (board_rsize(b) * board_rsize(b) * 100 / 164)    /*  19x19: 220 */
 
 #define check_special_sekis(b, m)  \
 	(b->moves > MOGGY_MIDDLEGAME && !immediate_liberty_count(b, m->coord))

uct/walk.c
@@ -353,7 +353,7 @@ scale_value(uct_t *u, board_t *b, enum stone node_color, tree_node_t *significan
 
 		int vp = u->val_points;
 		if (!vp) {
-			vp = board_size(b) - 1; vp *= vp; vp *= 2;
+			vp = board_stride(b) - 1; vp *= vp; vp *= 2;
 		}
 
 		floating_t sval = (floating_t) abs(result) / vp;
@@ -494,7 +494,7 @@ uct_playout_descent(uct_t *u, board_t *b, board_t *b2, enum stone player_color, 
 		significant[node_color - 1] = n;
 
 	int result;
-	int pass_limit = real_board_size2(b2) / 2;
+	int pass_limit = board_rsize(b2) * board_rsize(b2) / 2;
 	int passes = is_pass(last_move(b).coord) && b->moves > 0;
 
 	/* debug */

board.c
@@ -44,19 +44,19 @@ board_setup(board_t *b)
 }
 
 void
-board_init(board_t *b, int bsize, char *fbookfile)
+board_init(board_t *b, int size, char *fbookfile)
 {
 	board_setup(b);
 	b->fbookfile = fbookfile;
-	b->size = bsize;
+	b->rsize = size;
 	board_clear(b);	
 }
 
 board_t *
-board_new(int bsize, char *fbookfile)
+board_new(int size, char *fbookfile)
 {
 	board_t *b = malloc2(board_t);
-	board_init(b, bsize, fbookfile);
+	board_init(b, size, fbookfile);
 	return b;
 }
 
@@ -96,10 +96,10 @@ void
 board_resize(board_t *board, int size)
 {
 #ifdef BOARD_SIZE
-	assert(board_size(board) == size + 2);
+	assert(board_rsize(board) == size);
 #endif
 	assert(size <= BOARD_MAX_SIZE);
-	board->size = size + 2; /* S_OFFBOARD margin */
+	board->rsize = size;
 }
 
 board_statics_t board_statics = { 0, };
@@ -107,37 +107,39 @@ board_statics_t board_statics = { 0, };
 static void
 board_statics_init(board_t *board)
 {
-	int size = board_size(board);
+	int size = board_rsize(board);
+	int stride = size + 2;
 	board_statics_t *bs = &board_statics;
-	if (bs->size == size)
+	if (bs->rsize == size)
 		return;
 	
 	memset(bs, 0, sizeof(*bs));
-	bs->size = size;
-	bs->size2 = size * size;
-	bs->real_size2 = (size-2) * (size-2);
+	bs->rsize = size;
+	bs->stride = stride;
+	bs->max_coords = stride * stride;
 
 	bs->bits2 = 1;
-	while ((1 << bs->bits2) < bs->size2)  bs->bits2++;
+	while ((1 << bs->bits2) < bs->max_coords)  bs->bits2++;
 	
 	/* Setup neighborhood iterators */
-	bs->nei8[0] = -size - 1; // (-1,-1)
+	bs->nei8[0] = -stride - 1; // (-1,-1)
 	bs->nei8[1] = 1;
 	bs->nei8[2] = 1;
-	bs->nei8[3] = size - 2; // (-1,0)
+	bs->nei8[3] = stride - 2; // (-1,0)
 	bs->nei8[4] = 2;
-	bs->nei8[5] = size - 2; // (-1,1)
+	bs->nei8[5] = stride - 2; // (-1,1)
 	bs->nei8[6] = 1;
 	bs->nei8[7] = 1;
-	bs->dnei[0] = -size - 1;
+	
+	bs->dnei[0] = -stride - 1;
 	bs->dnei[1] = 2;
-	bs->dnei[2] = size*2 - 2;
+	bs->dnei[2] = stride*2 - 2;
 	bs->dnei[3] = 2;
 
 	/* Set up coordinate cache */
 	foreach_point(board) {
-		bs->coord[c][0] = c % board_size(board);
-		bs->coord[c][1] = c / board_size(board);
+		bs->coord[c][0] = c % stride;
+		bs->coord[c][1] = c / stride;
 	} foreach_point_end;
 
 	/* Initialize zobrist hashtable. */
@@ -173,33 +175,34 @@ board_statics_init(board_t *board)
 static void
 board_init_data(board_t *board)
 {
-	int size = board_size(board);
+	int size = board_rsize(board);
+	int stride = board_stride(board);
 
 	board_setup(board);
-	board_resize(board, size - 2 /* S_OFFBOARD margin */);
+	board_resize(board, size);
 
 	/* Setup initial symmetry */
 	if (size % 2) {
 		board->symmetry.d = 1;
-		board->symmetry.x1 = board->symmetry.y1 = board_size(board) / 2;
-		board->symmetry.x2 = board->symmetry.y2 = board_size(board) - 1;
+		board->symmetry.x1 = board->symmetry.y1 = stride / 2;
+		board->symmetry.x2 = board->symmetry.y2 = stride - 1;
 		board->symmetry.type = SYM_FULL;
 	} else {
 		/* TODO: We do not handle board symmetry on boards
 		 * with no tengen yet. */
 		board->symmetry.d = 0;
 		board->symmetry.x1 = board->symmetry.y1 = 1;
-		board->symmetry.x2 = board->symmetry.y2 = board_size(board) - 1;
+		board->symmetry.x2 = board->symmetry.y2 = stride - 1;
 		board->symmetry.type = SYM_NONE;
 	}
 
 	/* Draw the offboard margin */
-	int top_row = board_size2(board) - board_size(board);
+	int top_row = board_max_coords(board) - stride;
 	int i;
-	for (i = 0; i < board_size(board); i++)
+	for (i = 0; i < stride; i++)
 		board->b[i] = board->b[top_row + i] = S_OFFBOARD;
-	for (i = 0; i <= top_row; i += board_size(board))
-		board->b[i] = board->b[board_size(board) - 1 + i] = S_OFFBOARD;
+	for (i = 0; i <= top_row; i += stride)
+		board->b[i] = board->b[i + stride - 1] = S_OFFBOARD;
 
 	foreach_point(board) {
 		coord_t coord = c;
@@ -211,9 +214,11 @@ board_init_data(board_t *board)
 	} foreach_point_end;
 
 	/* All positions are free! Except the margin. */
-	for (i = board_size(board); i < (board_size(board) - 1) * board_size(board); i++)
-		if (i % board_size(board) != 0 && i % board_size(board) != board_size(board) - 1)
-			board_addf(board, i);
+	foreach_point(board) {
+		if (board_at(board, c) == S_NONE)
+			board_addf(board, c);
+	} foreach_point_end;
+	assert(board->flen == size * size);
 
 #ifdef BOARD_PAT3
 	/* Initialize 3x3 pattern codes. */
@@ -227,7 +232,7 @@ board_init_data(board_t *board)
 void
 board_clear(board_t *board)
 {
-	int size = board_size(board);
+	int size = board_rsize(board);
 	floating_t komi = board->komi;
 	char *fbookfile = board->fbookfile;
 	enum rules rules = board->rules;
@@ -236,8 +241,8 @@ board_clear(board_t *board)
 
 	board_statics_init(board);
 	static board_t bcache[BOARD_MAX_SIZE + 2];
-	assert(size > 0 && size <= BOARD_MAX_SIZE + 2);
-	if (bcache[size - 1].size == size)
+	assert(size > 1 && size <= BOARD_MAX_SIZE);
+	if (bcache[size - 1].rsize == size)
 		board_copy(board, &bcache[size - 1]);
 	else {
 		board_init_data(board);
@@ -255,17 +260,18 @@ board_clear(board_t *board)
 static void
 board_print_top(board_t *board, strbuf_t *buf, int c)
 {
+	int size = board_rsize(board);
 	for (int i = 0; i < c; i++) {
 		char asdf[] = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
 		sbprintf(buf, "      ");
-		for (int x = 1; x < board_size(board) - 1; x++)
+		for (int x = 1; x <= size; x++)
 			sbprintf(buf, "%c ", asdf[x - 1]);
 		sbprintf(buf, " ");
 	}
 	sbprintf(buf, "\n");
 	for (int i = 0; i < c; i++) {
 		sbprintf(buf, "    +-");
-		for (int x = 1; x < board_size(board) - 1; x++)
+		for (int x = 1; x <= size; x++)
 			sbprintf(buf, "--");
 		sbprintf(buf, "+");
 	}
@@ -275,9 +281,10 @@ board_print_top(board_t *board, strbuf_t *buf, int c)
 static void
 board_print_bottom(board_t *board, strbuf_t *buf, int c)
 {
+	int size = board_rsize(board);
 	for (int i = 0; i < c; i++) {
 		sbprintf(buf, "    +-");
-		for (int x = 1; x < board_size(board) - 1; x++)
+		for (int x = 1; x <= size; x++)
 			sbprintf(buf, "--");
 		sbprintf(buf, "+");
 	}
@@ -287,8 +294,9 @@ board_print_bottom(board_t *board, strbuf_t *buf, int c)
 static void
 board_print_row(board_t *board, int y, strbuf_t *buf, board_cprint cprint, void *data)
 {
+	int size = board_rsize(board);
 	sbprintf(buf, " %2d | ", y);
-	for (int x = 1; x < board_size(board) - 1; x++)
+	for (int x = 1; x <= size; x++)
 		if (coord_x(last_move(board).coord) == x && coord_y(last_move(board).coord) == y)
 			sbprintf(buf, "%c)", stone2char(board_atxy(board, x, y)));
 		else
@@ -296,7 +304,7 @@ board_print_row(board_t *board, int y, strbuf_t *buf, board_cprint cprint, void 
 	sbprintf(buf, "|");
 	if (cprint) {
 		sbprintf(buf, " %2d | ", y);
-		for (int x = 1; x < board_size(board) - 1; x++)
+		for (int x = 1; x <= size; x++)
 			cprint(board, coord_xy(x, y), buf, data);
 		sbprintf(buf, "|");
 	}
@@ -309,14 +317,18 @@ board_print_custom(board_t *board, FILE *f, board_cprint cprint, void *data)
 	char buffer[10240];
 	strbuf_t strbuf;
 	strbuf_t *buf = strbuf_init(&strbuf, buffer, sizeof(buffer));
+	int size = board_rsize(board);
+
 	sbprintf(buf, "Move: % 3d  Komi: %2.1f  Handicap: %d  Captures B: %d W: %d  ",
 		 board->moves, board->komi, board->handicap,
-		 board->captures[S_BLACK], board->captures[S_WHITE]);
+		 board->captures[S_BLACK], board->captures[S_WHITE]);	
+	
 	if (cprint) /* handler can add things to header when called with pass */
 		cprint(board, pass, buf, data);
 	sbprintf(buf, "\n");
+	
 	board_print_top(board, buf, 1 + !!cprint);
-	for (int y = board_size(board) - 2; y >= 1; y--)
+	for (int y = size; y >= 1; y--)
 		board_print_row(board, y, buf, cprint, data);
 	board_print_bottom(board, buf, 1 + !!cprint);
 	fprintf(f, "%s\n", buf->str);
@@ -325,8 +337,9 @@ board_print_custom(board_t *board, FILE *f, board_cprint cprint, void *data)
 static void
 board_hprint_row(board_t *board, int y, strbuf_t *buf, board_print_handler handler, void *data)
 {
+	int size = board_rsize(board);
         sbprintf(buf, " %2d | ", y);
-        for (int x = 1; x < board_size(board) - 1; x++) {
+	for (int x = 1; x <= size; x++) {
                 char *stone_str = handler(board, coord_xy(x, y), data);
                 if (coord_x(last_move(board).coord) == x && coord_y(last_move(board).coord) == y)
                         sbprintf(buf, "%s)", stone_str);
@@ -342,11 +355,14 @@ board_hprint(board_t *board, FILE *f, board_print_handler handler, void *data)
         char buffer[10240];
 	strbuf_t strbuf;
 	strbuf_t *buf = strbuf_init(&strbuf, buffer, sizeof(buffer));
+	int size = board_rsize(board);
+	
         sbprintf(buf, "Move: % 3d  Komi: %2.1f  Handicap: %d  Captures B: %d W: %d\n",
 		 board->moves, board->komi, board->handicap,
 		 board->captures[S_BLACK], board->captures[S_WHITE]);
+	
 	board_print_top(board, buf, 1);
-        for (int y = board_size(board) - 2; y >= 1; y--)
+	for (int y = size; y >= 1; y--)
                 board_hprint_row(board, y, buf, handler, data);
         board_print_bottom(board, buf, 1);
         fprintf(f, "%s\n", buf->str);
@@ -394,7 +410,7 @@ board_coord_in_symmetry(board_t *b, coord_t c)
 	if (b->symmetry.d) {
 		int x = coord_x(c);
 		if (b->symmetry.type == SYM_DIAG_DOWN)
-			x = board_size(b) - 1 - x;
+			x = board_stride(b) - 1 - x;
 		if (x > coord_y(c))
 			return false;
 	}
@@ -411,8 +427,8 @@ board_symmetry_update(board_t *b, board_symmetry_t *symmetry, coord_t c)
 		return;
 	}
 
-	int x = coord_x(c), y = coord_y(c), t = board_size(b) / 2;
-	int dx = board_size(b) - 1 - x; /* for SYM_DOWN */
+	int x = coord_x(c), y = coord_y(c), t = board_stride(b) / 2;
+	int dx = board_stride(b) - 1 - x; /* for SYM_DOWN */
 	if (DEBUGL(6))
 		fprintf(stderr, "SYMMETRY [%d,%d,%d,%d|%d=%d] update for %d,%d\n",
 			symmetry->x1, symmetry->y1, symmetry->x2, symmetry->y2,
@@ -427,28 +443,28 @@ board_symmetry_update(board_t *b, board_symmetry_t *symmetry, coord_t c)
 			if (x == y) {
 				symmetry->type = SYM_DIAG_UP;
 				symmetry->x1 = symmetry->y1 = 1;
-				symmetry->x2 = symmetry->y2 = board_size(b) - 1;
+				symmetry->x2 = symmetry->y2 = board_stride(b) - 1;
 				symmetry->d = 1;
 			} else if (dx == y) {
 				symmetry->type = SYM_DIAG_DOWN;
 				symmetry->x1 = symmetry->y1 = 1;
-				symmetry->x2 = symmetry->y2 = board_size(b) - 1;
+				symmetry->x2 = symmetry->y2 = board_stride(b) - 1;
 				symmetry->d = 1;
 			} else if (x == t) {
 				symmetry->type = SYM_HORIZ;
 				symmetry->y1 = 1;
-				symmetry->y2 = board_size(b) - 1;
+				symmetry->y2 = board_stride(b) - 1;
 				symmetry->d = 0;
 			} else if (y == t) {
 				symmetry->type = SYM_VERT;
 				symmetry->x1 = 1;
-				symmetry->x2 = board_size(b) - 1;
+				symmetry->x2 = board_stride(b) - 1;
 				symmetry->d = 0;
 			} else {
 break_symmetry:
 				symmetry->type = SYM_NONE;
 				symmetry->x1 = symmetry->y1 = 1;
-				symmetry->x2 = symmetry->y2 = board_size(b) - 1;
+				symmetry->x2 = symmetry->y2 = board_stride(b) - 1;
 				symmetry->d = 0;
 			}
 			break;
@@ -496,10 +512,10 @@ void
 board_handicap(board_t *board, int stones, move_queue_t *q)
 {
 	assert(stones <= 9);
-	int margin = 3 + (board_size(board) >= 13);
+	int margin = 3 + (board_rsize(board) >= 13);
 	int min = margin;
-	int mid = board_size(board) / 2;
-	int max = board_size(board) - 1 - margin;
+	int mid = board_stride(board) / 2;
+	int max = board_stride(board) - 1 - margin;
 	const int places[][2] = {
 		{ min, min }, { max, max }, { max, min }, { min, max }, 
 		{ min, mid }, { max, mid },
@@ -708,8 +724,9 @@ board_score(board_t *b, int scores[S_MAX])
 void
 board_print_official_ownermap(board_t *b, int *final_ownermap)
 {
-	for (int y = board_size(b) - 2; y >= 1; y--) {
-		for (int x = 1; x < board_size(b) - 1; x++) {
+	int size = board_rsize(b);
+	for (int y = size; y >= 1; y--) {
+		for (int x = 1; x <= size; x++) {
 			coord_t c = coord_xy(x, y);
 			char *chars = ".XO:";
 			fprintf(stderr, "%c ", chars[final_ownermap[c]]);
@@ -781,7 +798,7 @@ floating_t
 board_official_score(board_t *b, move_queue_t *dead)
 {
 	int dame, seki;
-	int ownermap[board_size2(b)];
+	int ownermap[board_max_coords(b)];
 	return board_official_score_details(b, dead, &dame, &seki, ownermap, NULL);
 }
 
@@ -961,10 +978,11 @@ board_capturable_add(board_t *board, group_t group, coord_t lib)
 #ifdef WANT_BOARD_C
 	/* Update the list of capturable groups. */
 	assert(group);
-	assert(board->clen < board_size2(board));
+	assert(board->clen < BOARD_MAX_GROUPS);
 	board->c[board->clen++] = group;
 #endif
 }
+
 static void
 board_capturable_rm(board_t *board, group_t group, coord_t lib)
 {
@@ -984,7 +1002,7 @@ board_capturable_rm(board_t *board, group_t group, coord_t lib)
 			board->c[i] = board->c[--board->clen];
 			return;
 		}
-	fprintf(stderr, "rm of bad group %d\n", group_base(group));
+	fprintf(stderr, "rm of bad group %s\n", coord2sstr(group_base(group)));
 	assert(0);
 #endif
 }
@@ -999,6 +1017,6 @@ board_play(board_t *b, move_t *m)
 #ifdef BOARD_UNDO_CHECKS
         assert(!b->quicked);
 #endif
-        
-        return board_play_(b, m);
+
+	return board_play_(b, m);
 }

board_play.h
@@ -29,7 +29,7 @@ static void
 board_group_find_extra_libs(board_t *board, group_t group, group_info_t *gi, coord_t avoid)
 {
 	/* Add extra liberty from the board to our liberty list. */
-	unsigned char watermark[board_size2(board) / 8];
+	unsigned char watermark[board_max_coords(board) / 8];
 	memset(watermark, 0, sizeof(watermark));
 #define watermark_get(c)	(watermark[c >> 3] & (1 << (c & 7)))
 #define watermark_set(c)	watermark[c >> 3] |= (1 << (c & 7))
@@ -156,11 +156,9 @@ add_to_group(board_t *board, group_t group, coord_t prevstone, coord_t coord)
 	});
 
 	if (DEBUGL(8))
-		fprintf(stderr, "add_to_group: added (%d,%d ->) %d,%d (-> %d,%d) to group %d\n",
-			coord_x(prevstone), coord_y(prevstone),
-			coord_x(coord), coord_y(coord),
-			groupnext_at(board, coord) % board_size(board), groupnext_at(board, coord) / board_size(board),
-			group_base(group));
+		fprintf(stderr, "add_to_group: added (%s ->) %s (-> %s) to group %s\n",
+			coord2sstr(prevstone), coord2sstr(coord), coord2sstr(groupnext_at(board, coord)),
+			coord2sstr(group_base(group)));
 }
 
 static void profiling_noinline

dcnn.c
@@ -20,7 +20,7 @@ static void detlef54_dcnn_eval(board_t *b, enum stone color, float result[]);
 static bool
 dcnn_supported_board_size(board_t *b)
 {
-	return (real_board_size(b) >= 13);
+	return (board_rsize(b) >= 13);
 }
 
 bool
@@ -35,7 +35,7 @@ void
 dcnn_init(board_t *b)
 {
 	if (dcnn_enabled && dcnn_supported_board_size(b))
-		caffe_init(real_board_size(b));
+		caffe_init(board_rsize(b));
 	if (dcnn_required && !caffe_ready())  die("dcnn required, aborting.\n");
 }
 
@@ -58,7 +58,7 @@ detlef54_dcnn_eval(board_t *b, enum stone color, float result[])
 {
 	assert(dcnn_supported_board_size(b));
 
-	int size = real_board_size(b);
+	int size = board_rsize(b);
 	int dsize = 13 * size * size;
 	float *data = calloc2(dsize, float);
 

dcnn.h
@@ -24,7 +24,7 @@ static inline int coord2dcnn_idx(coord_t c);
 static inline int
 coord2dcnn_idx(coord_t c)
 {
-	int size = the_real_board_size();
+	int size = the_board_rsize();
 	int x = coord_x(c) - 1;
 	int y = coord_y(c) - 1;
 	return (y * size + x);

commit 9e9435446ee6ee985a74bc0394309405c7e84300

board_earliest_pass(): fixed <new_board> PR bug, values were wrong !
was using board_max_coords() instead of board_rsize()^2
cleanup: put board_rsize2() + users back, for convenience

bug was introduced in 2da3fd0

board.h
@@ -237,6 +237,7 @@ FB_ONLY(int moveno)[BOARD_MAX_COORDS];     /* Move number for each coord */
 #endif
 
 #define board_stride(b)           (board_rsize(b) + 2)
+#define board_rsize2(b)		  (board_rsize(b) * board_rsize(b))
 
 
 /* This is a shortcut for taking different action on smaller and large boards 

ownermap.h
@@ -81,7 +81,7 @@ bool board_position_final_full(board_t *b, ownermap_t *ownermap,
 
 /* Don't allow passing earlier than that:
  * 19x19: 120    15x15: 56    13x13: 33    9x9: 16 */
-#define board_earliest_pass(b)  (board_max_coords(b) / (7 - 2 * board_rsize(b) / 9))
+#define board_earliest_pass(b)  (board_rsize2(b) / (7 - 2 * board_rsize(b) / 9))
 
 
 #endif

tactics/seki.h
@@ -1,8 +1,8 @@
 #ifndef PACHI_TACTICS_SEKI_H
 #define PACHI_TACTICS_SEKI_H
 
-#define MOGGY_MIDDLEGAME  (board_rsize(b) * board_rsize(b) * 10 / 25)      /*  19x19: 144 */
-#define MOGGY_ENDGAME     (board_rsize(b) * board_rsize(b) * 100 / 164)    /*  19x19: 220 */
+#define MOGGY_MIDDLEGAME  (board_rsize2(b) * 10 / 25)      /*  19x19: 144 */
+#define MOGGY_ENDGAME     (board_rsize2(b) * 100 / 164)    /*  19x19: 220 */
 
 #define check_special_sekis(b, m)  \
 	(b->moves > MOGGY_MIDDLEGAME && !immediate_liberty_count(b, m->coord))

uct/walk.c
@@ -488,7 +488,7 @@ uct_playout_descent(uct_t *u, board_t *b, board_t *b2, enum stone player_color, 
 		significant[node_color - 1] = n;
 
 	int result;
-	int pass_limit = board_rsize(b2) * board_rsize(b2) / 2;
+	int pass_limit = board_rsize2(b2) / 2;
 	int passes = is_pass(last_move(b).coord) && b->moves > 0;
 
 	/* debug */