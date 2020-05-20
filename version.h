fddc8583df91d073e0453226bf7175d897e723d1
lemonsqueeze <lemonsqueeze@gmx.com>
2020-05-20 07:41:58 +0200

    Pachi 12.50

--- version.h
+++ version.h
@@ -3,8 +3,8 @@
 
 #include "build.h"
 
-#define PACHI_VERNUM 12.45
-#define PACHI_VERNUMS "12.45"
+#define PACHI_VERNUM 12.50
+#define PACHI_VERNUMS "12.50"
 
 /* 00. Wang Zhi
  * 01. Sansa     Meijin  1612-1623
