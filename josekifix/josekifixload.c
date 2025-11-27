fab6684541320abea474d6076aed3bd157e70bfe
lemonsqueeze <lemonsqueeze@gmx.com>
2025-11-27 10:00:14 +0100

    josekifix: check gtp error on load

--- josekifix/josekifixload.c
+++ josekifix/josekifixload.c
@@ -490,8 +490,7 @@ josekifix_load(void)
 		
 		gtp.quiet = true;  // XXX fixme, refactor
 		enum parse_code c = gtp_parse(&gtp, b, &e, ti, buf);  /* quiet */
-		/* TODO check gtp command didn't gtp_error() also, will still return P_OK on error ... */
-		if (c != P_OK && c != P_ENGINE_RESET)
+		if (gtp.error || (c != P_OK && c != P_ENGINE_RESET))
 			die("%s:%i  gtp command '%s' failed, aborting.\n", fname, lineno, buf);
 	}
 	engine_done(&e);
