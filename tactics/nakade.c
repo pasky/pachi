9d7729a296a1534ae7fda7232fb9911f4be721bb
lemonsqueeze <lemonsqueeze@gmx.com>
2025-12-13 21:19:10 +0100

    nakade: use mq_t, added nakade_area_dead_shape()

--- tactics/nakade.c
+++ tactics/nakade.c
@@ -12,7 +12,7 @@
 
 
 static inline int
-nakade_area(board_t *b, coord_t around, enum stone color, coord_t *area)
+nakade_area(board_t *b, coord_t around, enum stone color, mq_t *area)
 {
 #ifdef EXTRA_CHECKS
 	assert(sane_coord(around));
@@ -22,45 +22,36 @@ nakade_area(board_t *b, coord_t around, enum stone color, coord_t *area)
 	/* First, examine the nakade area. For sure, it must be at most
 	 * six points. And it must be within color group(s). */
 #define NAKADE_MAX 6
-	int area_n = 0;
+	mq_init(area);
 
-	area[area_n++] = around;
+	mq_add(area, around);
 
-	for (int i = 0; i < area_n; i++) {
-		foreach_neighbor(b, area[i], {
+	for (int i = 0; i < area->moves; i++) {
+		foreach_neighbor(b, area->move[i], {
 			if (board_at(b, c) == stone_other(color))
 				return -1;
 			if (board_at(b, c) == S_NONE) {
-				bool dup = false;
-				for (int j = 0; j < area_n; j++)
-					if (c == area[j]) {
-						dup = true;
-						break;
-					}
-				if (dup) continue;
-
-				if (area_n >= NAKADE_MAX) {
-					/* Too large nakade area. */
+				mq_add_nodup(area, c);
+				if (area->moves > NAKADE_MAX)  /* Too large nakade area. */
 					return -1;
-				}
-				area[area_n++] = c;
 			}
 		});
 	}
 
-	return area_n;
+	return area->moves;
 }
 
 static inline void
-get_neighbors(board_t *b, coord_t *area, int area_n, int *neighbors, int *ptbynei)
+get_neighbors(board_t *b, mq_t *area, int *neighbors, int *ptbynei)
 {
 	/* We also collect adjecency information - how many neighbors
 	 * we have for each area point, and histogram of this. This helps
 	 * us verify the appropriate bulkiness of the shape. */
+	int area_n = area->moves;
         memset(neighbors, 0, area_n * sizeof(int));
 	for (int i = 0; i < area_n; i++) {
 		for (int j = i + 1; j < area_n; j++)
-			if (coord_is_adjecent(area[i], area[j])) {
+			if (coord_is_adjecent(area->move[i], area->move[j])) {
 				ptbynei[neighbors[i]]--;
 				neighbors[i]++;
 				ptbynei[neighbors[i]]++;
@@ -72,15 +63,15 @@ get_neighbors(board_t *b, coord_t *area, int area_n, int *neighbors, int *ptbyne
 }
 
 static inline coord_t
-nakade_point_(coord_t *area, int area_n, int *neighbors, int *ptbynei)
+nakade_point_(mq_t *area, int *neighbors, int *ptbynei)
 {
 	/* For each given neighbor count, arbitrary one coordinate
 	 * featuring that. */
 	coord_t coordbynei[9];
-	for (int i = 0; i < area_n; i++)
-		coordbynei[neighbors[i]] = area[i];
+	for (int i = 0; i < area->moves; i++)
+		coordbynei[neighbors[i]] = area->move[i];
 
-	switch (area_n) {
+	switch (area->moves) {
 		case 1: return pass;
 		case 2: return pass;
 		case 3: assert(ptbynei[2] == 1);
@@ -107,17 +98,34 @@ nakade_point(board_t *b, coord_t around, enum stone color)
 	assert(is_player_color(color));
 	assert(board_at(b, around) == S_NONE);
 #endif
-	coord_t area[NAKADE_MAX]; int area_n = 0;
-	area_n = nakade_area(b, around, color, area);
+	mq_t area;
+	int area_n = nakade_area(b, around, color, &area);
 	if (area_n == -1)
 		return pass;
 
 	int neighbors[area_n]; int ptbynei[9] = {area_n, 0};
-	get_neighbors(b, area, area_n, neighbors, ptbynei);
+	get_neighbors(b, &area, neighbors, ptbynei);
 	
-	return nakade_point_(area, area_n, neighbors, ptbynei);
+	return nakade_point_(&area, neighbors, ptbynei);
 }
 
+bool
+nakade_area_dead_shape(board_t *b, mq_t *area)
+{
+	int area_n = area->moves;
+	if (area_n <= 3)		return true;
+	if (area_n > NAKADE_MAX)	return false;
+
+	int neighbors[area_n]; int ptbynei[9] = {area_n, 0};
+	get_neighbors(b, area, neighbors, ptbynei);
+
+	if (area_n == 4 && ptbynei[2] == 4)  // square 4
+		return true;
+
+	/* nakade_point() should be able to deal with the rest ... */
+	coord_t nakade = nakade_point_(area, neighbors, ptbynei);
+	return  (nakade != pass);
+}
 
 bool
 nakade_dead_shape(board_t *b, coord_t around, enum stone color)
@@ -127,18 +135,9 @@ nakade_dead_shape(board_t *b, coord_t around, enum stone color)
 	assert(is_player_color(color));
 	assert(board_at(b, around) == S_NONE);
 #endif
-	coord_t area[NAKADE_MAX]; int area_n = 0;
-	area_n = nakade_area(b, around, color, area);
-	if (area_n == -1)	return false;
-	if (area_n <= 3)	return true;
-	
-	int neighbors[area_n]; int ptbynei[9] = {area_n, 0};
-	get_neighbors(b, area, area_n, neighbors, ptbynei);
-	
-	if (area_n == 4 && ptbynei[2] == 4)  // square 4
-		return true;
-	
-	/* nakade_point() should be able to deal with the rest ... */
-	coord_t nakade = nakade_point_(area, area_n, neighbors, ptbynei);
-	return  nakade != pass;
+	mq_t area;
+	int area_n = nakade_area(b, around, color, &area);
+	if (area_n == -1)
+		return false;
+	return nakade_area_dead_shape(b, &area);
 }
