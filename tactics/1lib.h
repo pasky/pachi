71ae7e79f6c144b629164648b05a326dc8ca4f12
lemonsqueeze <lemonsqueeze@gmx.com>
2026-01-30 09:34:06 +0100

    tactics: 1lib.c cleanup
    EXTRA_CHECKS fixes
    can_play_on_lib(): check suicide before selfatari (speedup)

--- tactics/1lib.h
+++ tactics/1lib.h
@@ -6,15 +6,18 @@
 #include "board.h"
 #include "debug.h"
 
-/* Can capture group g (not snapback) */
+/********************************************************************************/
+/* 1 lib tactical checks */
+
+/* Can capture group g (not snapback).
+ * Note: if capturing group is not in atari use board_is_valid_play() instead, faster. */
 bool can_capture(board_t *b, group_t g, enum stone to_play);
 
 /* Check if @group capture is a snapback.
  * see also is_snapback(): faster for checking a potential move than
  * with_move(selfatari) + capturing_group_is_snapback() */
 bool capturing_group_is_snapback(board_t *b, group_t group);
-/* Can group @group usefully capture a neighbor ? 
- * (usefully: not a snapback) */
+/* Can group @group usefully capture a neighbor ? (usefully: not a snapback) */
 bool can_countercapture(board_t *b, group_t group, mq_t *q);
 /* Same as can_countercapture() but returns capturable groups instead of moves. */
 bool countercapturable_groups(board_t *b, group_t group, mq_t *q);
